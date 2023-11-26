4746fd1eea02b4ea47a400a72c87fef039771fb0
eshad <fidaeshad10@gmail.com>
2023-11-26 03:15:51 -0600

    first commit

---
+++ example/anonymousfunctions_filteringfruits_example.dart
@@ -0,0 +1,6 @@
+import 'package:anonymousfunctions_filteringfruits/anonymousfunctions_filteringfruits.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
