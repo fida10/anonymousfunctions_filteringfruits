812d818d21de81ad1559ca934ac8f6b23f202aba
eshad <fidaeshad10@gmail.com>
2023-11-26 03:30:39 -0600

    finished question with foreach

--- lib/anonymousfunctions_filteringfruits.dart
+++ lib/anonymousfunctions_filteringfruits.dart
@@ -5,3 +5,29 @@ library;
 
 export 'src/anonymousfunctions_filteringfruits_base.dart';
 
+/*
+Given the following list of fruits:
+
+final fruits = {
+  'apple': 40,
+  'banana': 25,
+  'cherry': 15,
+  'date': 50,
+  'elderberry': 30
+};
+
+Use Dart's higher-order functions to calculate the total number
+of fruits that have names longer than 5 characters. 
+How many apples, bananas, and elderberries are there in total?
+ */
+
+int countLongNameFruits(Map<String, int> fruits){
+  int counter = 0;
+  fruits.forEach((key, value) { 
+    if(key.length > 5){
+      counter += value;
+    }
+  });
+
+  return counter;
+}
